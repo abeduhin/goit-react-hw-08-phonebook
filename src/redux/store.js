8377399a6fcce21ff7f90dd97f806b5ba97dc11c
Oleksii Biedukhin <abeduhin17@gmail.com>
2023-01-20 14:00:03 +0200

    test

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlise';
 import { filterReducer } from './filterSlise';
 
+
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
